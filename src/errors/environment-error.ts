bdc1ba7d516e4070128733c9c34b2788e25b4781
Hawre Aziz <hawre.aziz@volvocars.com>
2021-09-26 18:10:38 +0200

    update

--- src/errors/environment-error.ts
+++ src/errors/environment-error.ts
@@ -5,14 +5,14 @@ export class EnvironmentError extends CustomError {
   statusCode = 500
   envVariable: string;
   constructor(envVariable: string) {
-    super(`${envVariable} not found`);
+    super(`${envVariable} is not set`);
     Object.setPrototypeOf(this, EnvironmentError.prototype);
     this.envVariable = envVariable
   }
 
   serializeError() {
     return [
-      { message: `${this.envVariable} is not set` }
+      { message: this.message }
     ];
   }
 }
